57003d6771335821e1b175b50efc25eef55a7fbf
Manoel Duran <manoel.duran@hotmail.com>
2022-06-24 15:30:20 -0300

    api key

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Event from "../pages/Event";
+import Subscribe from "../pages/Subscribe";
 
 const Router = () => {
     return (
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
-            <Route path="/event" element={<Event/>}/>
-            <Route path="/event/lesson/:slug" element={<Event/>}/>
+            <Route path="/" element={<Subscribe />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/event/lesson/:slug" element={<Event />} />
         </Routes>
     );
 }
