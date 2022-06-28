4bc75133f1b5042cf98afa52c1e7ad6ba2135d36
Manoel Duran <manoel.duran@hotmail.com>
2022-06-28 20:55:19 -0300

    chore: changed title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
 return (
    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      <h1 className="text-2xl font-bold">Manubas Videos</h1>
+      <h1 className="text-2xl font-bold">Manubas Vídeos</h1>
    </header>
 );
 }
